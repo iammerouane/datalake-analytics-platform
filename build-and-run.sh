5683e440145e327eb9fe3dc0f7db12252165ee83
Merouane Bennaceur <30907855+merouanebe@users.noreply.github.com>
2023-10-09 08:13:40 +0200

    Initial Commit

---
+++ build-and-run.sh
@@ -0,0 +1,15 @@
+#Create hadoop base image
+cd hadoop/base && docker build . -t hadoop-base && cd ../..
+#Create spark base image
+cd spark/base && docker build . -t spark-base && cd ../..
+#Start all containers
+docker compose up -d
+
+sleep 30
+
+docker exec -it hadoop-namenode hdfs --loglevel FATAL dfs -copyFromLocal /data/bikes.csv /
+docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/bikeshops.csv /
+docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/orders.csv /
+docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/bikes.xlsx /
+docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/bikeshops.xlsx /
+docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/orders.xlsx /
