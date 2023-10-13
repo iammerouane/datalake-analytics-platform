3d45e1a36254079a87140229cc5fd6be87b0b792
Merouane Bennaceur <nebula@vmi1478507.contaboserver.net>
2023-10-13 07:56:25 +0200

    Final Commit

--- build-and-run.sh
+++ build-and-run.sh
@@ -7,11 +7,19 @@ docker compose build
 #Start all containers
 docker compose up -d
 
-sleep 30
 
-docker exec -it hadoop-namenode hdfs --loglevel FATAL dfs -copyFromLocal /data/bikes.csv /
+echo "Chargement des fichiers dans HDFS ..."
+echo "(assurez vous d'avoir mis les fichiers csv dans le dossier data)"
+sleep 60
+
+# Chargement des fichiers CSV dans HDFS
+docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/bikes.csv /
 docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/bikeshops.csv /
 docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/orders.csv /
-docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/bikes.xlsx /
-docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/bikeshops.xlsx /
-docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/orders.xlsx /
+docker exec -it hadoop-namenode hdfs dfs -copyFromLocal /data/customers.csv /
+
+# Récupération de l'adresse de la BD MYSQL
+ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql)
+
+# Execution du script PySpark
+docker exec -it jupyter-notebook python run.py $ip
